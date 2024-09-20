347adeff7065b1f1e5fd06718912b04296eca286
Kenneth Murray <kwmwins@gmail.com>
2024-09-19 17:24:13 -0700

    Create text_utils.py

---
+++ text_utils.py
@@ -0,0 +1,8 @@
+
+def reverse_string(s):
+    """Function to reverse a given string."""
+    return s[::-1]
+
+def capitalize_string(s):
+    """Function to capitalize a given string."""
+    return s.capitalize()
